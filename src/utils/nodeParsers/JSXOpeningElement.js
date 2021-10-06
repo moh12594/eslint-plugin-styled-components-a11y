dd7d7b614b3c01d0cf6c41769fcfe7a12fd92b78
Mohamed Sadat <mohamedsadat@momo.home>
2021-10-06 17:37:03 +0200

    feat: add bem styling support

--- src/utils/nodeParsers/JSXOpeningElement.js
+++ src/utils/nodeParsers/JSXOpeningElement.js
@@ -6,7 +6,9 @@ module.exports = (context, styledComponents, rule, name) => ({
   JSXOpeningElement(node) {
     const func = inspectee => name.includes('scope') && context.report(node, inspect(inspectee || node));
     try {
-      const originalName = node.name.name;
+      const originalName = node.name.type === 'JSXMemberExpression'
+        ? `${node.name.object.name}.${node.name.property.name}`
+        : node.name.name;
       const styledComponent = styledComponents[originalName];
       if (styledComponent) {
         const { tag, attrs } = styledComponent;
@@ -15,10 +17,8 @@ module.exports = (context, styledComponents, rule, name) => ({
           const allAttrs = mergeStyledAttrsWithNodeAttrs(attrs, originalNodeAttr);
           const asProp = getAsProp(allAttrs);
           node.attributes = allAttrs;
-          node.name.name = asProp || tag;
-          rule.create(context).JSXOpeningElement(node);
+          rule.create(context).JSXOpeningElement({ ...node, name: { name: asProp || tag, type: 'JSXIdentifier' } });
         } finally {
-          node.name.name = originalName;
           node.attributes = originalNodeAttr;
         }
       }
